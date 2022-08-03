35492ccfe0320f22b6190f6717fec6dc597cbadd
Joshua Lansford <HootOwl7777@gmail.com>
2022-08-03 17:01:17 -0400

    Added gradio demo

--- split_test_2.py
+++ split_test_2.py
@@ -17,6 +17,24 @@ def load_file( filename ):
     return result
 
 
+
+#edit distance from https://stackoverflow.com/a/32558749/1419054
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+
 def strip_whitespace( content ):
     return content.replace( " ", "" ).replace( "\n", "" )
 
@@ -57,7 +75,7 @@ def parse_for_training( lines ):
         
     return pd.DataFrame( context_split_into_dict ), pd.DataFrame( {'predict_space':answers} )
 
-def train_catboost( X_train,X_test, y_train, y_test, iterations ):
+def train_catboost( X_train,X_test, y_train, y_test, iterations, learning_rate = .07 ):
     train_pool = Pool(
         data=X_train,
         label=y_train,
@@ -70,7 +88,7 @@ def train_catboost( X_train,X_test, y_train, y_test, iterations ):
     )
     model = CatBoostClassifier(
         iterations = iterations,
-        learning_rate = .07
+        learning_rate = learning_rate
     )
     model.fit( train_pool, eval_set=validation_pool, verbose=True )
 
@@ -142,8 +160,54 @@ def main( iterations = 50 ):
     with open( f"split_output2_{iterations}.txt", "wt" ) as fout:
         fout.write( white_space_added )
     
+def gradio_demo(iterations):
+    import gradio as gr 
+
+    #first load all the bible in
+    full_bible = load_file( bible_filename )
+    #just train on all of it because the test is playing with it
+    bible_for_training = full_bible
+
+    model_filename = f"split_test2_model_full_{iterations}.cb"
+
+        #see if the model exists yet.
+    if not os.path.exists( model_filename ):
+
+        #parse the training side into structure which can train a model
+        X,y = parse_for_training( bible_for_training )
+
+        #split it between train and test.
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+            X,
+            y,
+            random_state=1,
+        )
+        X_train = X_train.reset_index(drop=True)
+        X_test = X_test.reset_index(drop=True)
+        y_train = y_train.reset_index(drop=True)
+        y_test = y_test.reset_index(drop=True)
+
+        #train catboost model
+        model = train_catboost( X_train,X_test, y_train, y_test, iterations )
+
+        #save the model
+        model.save_model( model_filename )
+    else:
+        model = CatBoostClassifier().load_model(  model_filename )
+
+    
+    def gradio_function( text ):
+        text = text.replace( " ", "" )
+        return add_whitespace( model, text, quite=True )
+
+    with gr.Blocks() as demo:
+        inp = gr.Textbox( label="Input" )
+        out = gr.Textbox( label="Output" )
+        inp.change( gradio_function, inputs=[inp], outputs=[out] )
+    demo.launch( share=True )
 
 if __name__ == '__main__': 
-    main(200)
+    #main(200)
     # for i in range(10):
     #     main((i+1)*10)
+    gradio_demo(200)
