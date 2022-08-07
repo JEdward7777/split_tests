926dccffa9fa680dedd01471ca0f4e5fc7a7705b
Joshua Lansford <HootOwl7777@gmail.com>
2022-08-07 11:23:04 -0400

    Code I found which was forgotten to be checked in.

--- swahili_split_audio.py
+++ swahili_split_audio.py
@@ -18,9 +18,11 @@ def main(iterations):
     data_for_spacing = list(data['allosaurus_transcript_no_spaces'])
     data_for_spacing = [s if isinstance(s,str) else "" for s in data_for_spacing]
 
+    model_name = f"{test_name}_model_{iterations}.cb"
+
 
     #see if the model exists yet.
-    if not os.path.exists( f"{test_name}_model_{iterations}.cb" ):
+    if not os.path.exists( model_name ):
 
         #parse the training side into structure which can train a model
         X,y = split_test_2.parse_for_training( data_for_training )
@@ -40,23 +42,43 @@ def main(iterations):
         model = split_test_2.train_catboost( X_train,X_test, y_train, y_test, iterations )
 
         #save the model 
-        model.save_model( f"{test_name}_model_{iterations}.cb" )
+        model.save_model( model_name )
 
         #200 test of .0434672
         #500 test of .0379901
     else:
-        model = CatBoostClassifier().load_model(  f"{test_name}_model_{iterations}.cb" )
+        model = CatBoostClassifier().load_model(  model_name )
 
     
     #Run the model and add a column on the dataset and save it back out.
     respaced_result = []
+    edit_distances_to_with_spaces = []
+    reductions_per_space = []
+    reductions = []
     for i,audio_line in enumerate(data_for_spacing):
         spaces_added = split_test_2.add_whitespace( model, audio_line, quite=True )
-        print(  f"{i} of {len(data_for_spacing)}: {spaces_added}\n" )
         respaced_result.append( spaces_added )
 
+        #see if the spaces helped the edit distance.
+        edit_distance_before = split_test_2.levenshteinDistance( data_for_training[i], audio_line )
+        edit_distance_after = split_test_2.levenshteinDistance( data_for_training[i], spaces_added )
+        reduction = edit_distance_before-edit_distance_after
+        edit_distances_to_with_spaces.append( edit_distance_after )
+
+        num_spaces_added = len( spaces_added ) - len( audio_line )
+        reduction_per_space = reduction/num_spaces_added if num_spaces_added > 0 else 0
+        reductions_per_space.append(reduction_per_space)
+        reductions.append(reduction)
+
+        print(  f"{i} of {len(data_for_spacing)}: edit reduction {reduction}, per space {reduction_per_space},  num_added {num_spaces_added} {spaces_added}\n" )
+
     data['allosaurus_transcript_spaced'] = respaced_result
+    data['allosaurus_transcript_spaced_edit_distance'] = edit_distances_to_with_spaces
+    data['allosaurus_transcript_edit_reduction' ] = reductions
+    data['allosaurus_transcript_edit_reduce_per_space' ] = reductions_per_space
     data.to_csv( f"{test_name}_{iterations}.csv" )
 
+    print( f"finished {test_name} with {iterations} iterations" )
+
 if __name__ == "__main__":
-    main( 1000 )
+    main( 8000 )
--- swahili_split_test.py
+++ swahili_split_test.py
@@ -7,24 +7,8 @@ from catboost import CatBoostClassifier
 data_filename = './data/ALFFA_dataset_ allosaurus vs epitran.csv'
 test_name = "swahili_test"
 
-#edit distance from https://stackoverflow.com/a/32558749/1419054
-def levenshteinDistance(s1, s2):
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
 
-    distances = range(len(s1) + 1)
-    for i2, c2 in enumerate(s2):
-        distances_ = [i2+1]
-        for i1, c1 in enumerate(s1):
-            if c1 == c2:
-                distances_.append(distances[i1])
-            else:
-                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
-        distances = distances_
-    return distances[-1]
-
-
-def main(iterations):
+def main(iterations, learning_rate):
 
     #print( data.describe() )
 
@@ -39,8 +23,10 @@ def main(iterations):
     data_for_training = full_data[:lines_for_training]
     data_for_testing = full_data[lines_for_training:]
 
+    model_name = f"{test_name}_model_{iterations}_{learning_rate}.cb"
+
     #see if the model exists yet.
-    if not os.path.exists( f"{test_name}_model_{iterations}.cb" ):
+    if not os.path.exists( model_name ):
 
         #parse the training side into structure which can train a model
         X,y = split_test_2.parse_for_training( data_for_training )
@@ -60,25 +46,30 @@ def main(iterations):
         model = split_test_2.train_catboost( X_train,X_test, y_train, y_test, iterations )
 
         #save the model 
-        model.save_model( f"{test_name}_model_{iterations}.cb" )
+        model.save_model( model_name )
 
         #200 test of .0434672
         #500 test of .0379901
     else:
-        model = CatBoostClassifier().load_model(  f"{test_name}_model_{iterations}.cb" )
+        model = CatBoostClassifier().load_model(  model_name )
 
     
     #Test model on remaining data and save it out to csv file for results.
-    with open( f"{test_name}_{iterations}.csv", "wt" ) as fout:
+    edit_distance_sum = 0
+    with open( f"{test_name}_{iterations}_{learning_rate}.csv", "wt" ) as fout:
         fout.write( "truth,output,edit distance\n" )
 
         for i,testing_line in enumerate(data_for_testing):
             without_whitespace = split_test_2.strip_whitespace( testing_line )
             spaces_added = split_test_2.add_whitespace( model, without_whitespace, quite=True )
-            edit_distance = levenshteinDistance( testing_line, spaces_added )
+            edit_distance = split_test_2.levenshteinDistance( testing_line, spaces_added )
             result = f"{testing_line},{spaces_added},{edit_distance}"
             fout.write( f"{result}\n" )
             print(  f"{i} of {len(data_for_testing)}: {result}\n" )
+
+            edit_distance_sum += edit_distance
+
+    print( f"Finished {iterations} iterations with a learning_rate of {learning_rate} and got average edit_distance of {edit_distance_sum/len(data_for_testing)}")
 
 
     # #test model on remaining data.
@@ -91,4 +82,9 @@ def main(iterations):
     
 
 if __name__ == '__main__': 
-    main(400)
+    #main(1000,.07)
+    #main(1000,.04)
+    #main(1000,.1)
+    #main(1000,.15)
+    #main(1000,.2)
+    main(8000,.07)
